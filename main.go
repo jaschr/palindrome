2871958f3109be791485b0472eeb640305bfd1b2
Jacob Schroeder <me@jaschr.com>
2022-11-05 23:16:50 -0400

    update

--- main.go
+++ main.go
@@ -5,48 +5,47 @@
  * Github Repo: https://github.com/jaschr/palindrome
  * Documentation: https://github.com/jaschr/palindrome/tree/main/docs
  */
- 
+
 package main
 
 import (
-  "fmt"
-  "log"
-  "os"
-  "path/filepath"
-  "strings"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func WalkMatch(root, pattern string) ([]string, error) {
-  var matches []string
-  err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-    if err != nil {
-      return err
-    }
-    if info.IsDir() {
-      return nil
-    }
-    if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
-      return err
-    } else if matched {
-      matches = append(matches, path)
-    }
-    return nil
-  })
-  if err != nil {
-    return nil, err
-  }
-  return matches, nil
-} 
-
-func main() {
-  root := "posts/"
-  files, err := WalkMatch("./" + root, "*.md")
-  if err != nil {
-    log.Fatal(err)
-  }
-  for _, f:= range files {
-    mdf := strings.ReplaceAll(f, root, "")
-    fmt.Println(mdf)
-  }
+	var matches []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
+			return err
+		} else if matched {
+			matches = append(matches, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return matches, nil
 }
 
+func main() {
+	root := "posts/"
+	files, err := WalkMatch("./"+root, "*.md")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		mdf := strings.ReplaceAll(f, root, "")
+		fmt.Println(mdf)
+	}
+}
